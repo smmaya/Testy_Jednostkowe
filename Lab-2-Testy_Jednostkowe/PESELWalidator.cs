e36ae142b71f43e433784aa0ee7db10b877e9edc
Sławomir Majchrzak <smmaya@yahoo.fr>
2022-04-24 17:30:42 +0200

    zadanko

---
+++ Lab-2-Testy_Jednostkowe/PESELWalidator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lab_2_Testy_Jednostkowe
+{ 
+    public class PESELWalidator
+    {
+        protected int[] wagi = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+        protected int[] pesel;
+        private string numerPesel;
+
+        public PESELWalidator(String pesel)
+        {
+            WczytajPesel(pesel);
+        }
+
+        public void WczytajPesel(String pesel)
+        {
+            numerPesel = pesel;
+        }
+
+        public int SumaKontrolna()
+        {
+            int suma = 0;
+
+            for (int i = 0; i < numerPesel.Length-1; i++)
+            {
+                suma += int.Parse(numerPesel[i].ToString()) * wagi[i];
+            }
+            return (10 - (suma % 10)) % 10;
+        }
+
+        public String DataUrodzenia()
+        {
+            return numerPesel.Substring(0, 2);;
+        }
+
+        public String Plec()
+        {
+            if (Convert.ToInt32(numerPesel[9]) % 2 == 0)
+                return "k";
+            return "m";
+        }
+
+        public Boolean SprawdzPesel()
+        {
+            string pattern = @"^[0-9]{11}$";
+            return Regex.Match(numerPesel, pattern).Value.Length == 11;
+        }
+
+    }
+}
---
+++ Lab-2-Testy_Jednostkowe/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab_2_Testy_Jednostkowe
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        PESELWalidator x, y, z;
+
+        [TestInitialize]
+        public void Inicjalizajca()
+        {
+            x = new PESELWalidator("73020380230");
+            y = new PESELWalidator("NNR3746547");
+            z = new PESELWalidator("73020380231");
+        }
+
+        [TestMethod]
+        public void TestSumaKontrolna()
+        {
+            Assert.AreEqual(0, x.SumaKontrolna());
+        }
+
+        [TestMethod]
+        public void TestDatyUrodzenia()
+        {
+            Assert.AreEqual("73", x.DataUrodzenia());
+        }
+
+        [TestMethod]
+        public void TestPleci()
+        {
+            Assert.AreEqual("m", x.Plec());
+        }
+
+        [TestMethod]
+        public void TestSprawdzPesel()
+        {
+            Assert.AreEqual(true, x.SprawdzPesel());
+        }
+
+        [TestMethod]
+        public void TestSprawdzNiepoprawnoscPesela()
+        {
+            Assert.AreEqual(false, y.SprawdzPesel());
+        }
+
+        [TestMethod]
+        public void TestSprawdzBlednaSumePesela()
+        {
+            Assert.AreEqual(0, z.SumaKontrolna());
+        }
+
+    }
+
+}
